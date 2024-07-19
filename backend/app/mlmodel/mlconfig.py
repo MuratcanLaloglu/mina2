b7c0bf9690139573358d73d1607f1bce8718ecc4
MuratcanLaloglu <quaristr@gmail.com>
2024-07-19 14:01:35 +0300

    Completed majority of backend functionality

--- backend/app/mlmodel/mlconfig.py
+++ backend/app/mlmodel/mlconfig.py
@@ -1,9 +1,13 @@
+import os
+
 import joblib
 
+this_dir = os.path.dirname(__file__)
 
 def load_model(model_name):
-    return joblib.load(f"./models/{model_name}.pkl")
+    return joblib.load(f"{this_dir}/models/{model_name}.pkl")
 
 
 def load_scaler(model_name):
-    return joblib.load(f"backend/app/mlmodel/models/Scaler{model_name[-1]}.pkl")
+    return joblib.load(f"{this_dir}/models/Scaler{model_name[-1]}.pkl")
+
