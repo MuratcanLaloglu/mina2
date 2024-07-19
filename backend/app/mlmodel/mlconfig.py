9767f63e452712c466e94999210c62209a50ba9d
MuratcanLaloglu <quaristr@gmail.com>
2024-07-19 15:05:36 +0300

    pass all tests #1

--- backend/app/mlmodel/mlconfig.py
+++ backend/app/mlmodel/mlconfig.py
@@ -1,13 +1,14 @@
 import os
+from typing import Any
 
-import joblib
+import joblib  # type: ignore
 
-this_dir = os.path.dirname(__file__)
+this_dir: str = os.path.dirname(__file__)
 
-def load_model(model_name):
+
+def load_model(model_name: str) -> Any:
     return joblib.load(f"{this_dir}/models/{model_name}.pkl")
 
 
-def load_scaler(model_name):
+def load_scaler(model_name: str) -> Any:
     return joblib.load(f"{this_dir}/models/Scaler{model_name[-1]}.pkl")
-
